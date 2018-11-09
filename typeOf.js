a07ea9bece0f773f7cd6df9fc5c6bec54eec6f57
Oyedele Hammed <itz.harmid@gmail.com>
2018-11-09 12:05:42 +0100

    Fixed spacing

--- typeOf.js
+++ typeOf.js
@@ -3,7 +3,8 @@
   * @version 1.0
   * @since January 21, 2018
   * @see https://github.com/Horlahcoded/typeOf.js
-*/
+  */
+
 function typeOf( value ) {
   return {}.toString.call( value ).slice( 8, -1 ).toLowerCase();
 }
